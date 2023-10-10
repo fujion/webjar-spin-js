29cbcc5573751acaf831f0460f9967f509c01144
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 18:12:43 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,7 +0,0 @@
-file_exists "import-map.json"
-file_exists "README.md"
-file_contains "LICENSE.md" "MIT License"
-
-file_exists "dist/spin.css"
-file_exists "dist/spin.js"
-file_exists "dist/spin.umd.js"
